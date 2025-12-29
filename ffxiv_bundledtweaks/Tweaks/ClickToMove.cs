b898a41d58b6cc2cf9626ba6a8bd1194290c2f31
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2025-12-29 13:44:03 +0000

    add map support to clicktomove

--- ffxiv_bundledtweaks/Tweaks/ClickToMove.cs
+++ ffxiv_bundledtweaks/Tweaks/ClickToMove.cs
@@ -1,7 +1,5 @@
 ﻿using Dalamud.Bindings.ImGui;
-using Dalamud.Utility;
 using ECommons;
-using ECommons.MathHelpers;
 using FFXIVClientStructs.FFXIV.Client.System.Framework;
 using FFXIVClientStructs.FFXIV.Client.UI;
 using FFXIVClientStructs.FFXIV.Client.UI.Agent;
@@ -16,57 +14,59 @@ public class ClickToMoveConfiguration {
 [Tweak]
 public unsafe class ClickToMove : Tweak<ClickToMoveConfiguration> {
     public override string Name => "Click to Move";
-    public override string Description => "Like those other games.";
+    public override string Description => "Like those other games. Supports clicking on the map.";
 
     private OverrideMovement movement = null!;
 
     public override void Enable() {
-        Svc.Framework.Update += MoveTo;
         movement = new();
-        //Svc.AddonLifecycle.RegisterListener(AddonEvent.PostReceiveEvent, "AreaMap", Logger);
-    }
-
-    private void Logger(AddonEvent type, AddonArgs args) {
-        if (args is AddonReceiveEventArgs { AtkEventType: (byte)AtkEventType.MouseDown } receiveArgs) {
-            if (receiveArgs.AtkEventData.As<AtkEventData.AtkMouseData>()->ButtonId != 0) return; // left click only
-            if (AgentMap.Instance()->CurrentMapId != AgentMap.Instance()->SelectedMapId) return;
-            if (args.GetAddon<AddonAreaMap>()->GetMouseWorldCoords() is { } coords)
-                Svc.Navmesh.PathfindAndMoveTo(coords.OnMesh(), Player.CanFly);
-        }
+        Svc.Framework.Update += MoveTo;
+        Svc.AddonLifecycle.RegisterListener(AddonEvent.PostReceiveEvent, "AreaMap", HandleMapClick);
     }
 
     public override void Disable() {
-        Svc.Framework.Update -= MoveTo;
         movement.Dispose();
-        //Svc.AddonLifecycle.UnregisterListener(Logger);
+        Svc.Framework.Update -= MoveTo;
+        Svc.AddonLifecycle.UnregisterListener(HandleMapClick);
     }
 
-    private bool isPressed = false;
-    private Vector3 destination = Vector3.Zero;
+    private void HandleMapClick(AddonEvent type, AddonArgs args) {
+        if (args is AddonReceiveEventArgs { AtkEventType: (byte)AtkEventType.MouseDown } receiveArgs) {
+            if (receiveArgs.AtkEventData.As<AtkEventData.AtkMouseData>()->ButtonId != 0) return; // left click only
+            if (AgentMap.Instance()->CurrentMapId != AgentMap.Instance()->SelectedMapId) return;
+            if (args.GetAddon<AddonAreaMap>()->GetMouseWorldCoords() is { } coords) {
+                if (Config.MovementType is MovementType.Pathfind)
+                    Svc.Navmesh.PathfindAndMoveTo(coords.OnMesh(), Player.CanFly);
+                else {
+                    movement.Enabled = true;
+                    movement.DesiredPosition = new(coords.X, Player.Position.Y, coords.Y);
+                }
+            }
+        }
+    }
+
+    private bool wasPressed = false;
     private void MoveTo(IFramework framework) {
         if (!Player.Available || Player.IsBusy) return;
-        if (Player.DistanceTo(destination) < 0.0025f) movement.Enabled = false;
 
-        if (IsKeyPressed(ECommons.Interop.LimitedKeys.LeftMouseButton) && Utils.IsClickingInGameWorld()) {
-            if (!isPressed)
-                isPressed = true;
+        if (Config.MovementType != MovementType.Pathfind && Player.Object.FlatDistanceTo(movement.DesiredPosition) < 0.05f) {
+            movement.Enabled = false;
         }
-        else {
-            if (isPressed) {
-                isPressed = false;
-                if (!Framework.Instance()->WindowInactive) {
-                    Svc.GameGui.ScreenToWorld(ImGui.GetIO().MousePos, out var pos, 100000f);
-                    if (Config.MovementType == MovementType.Pathfind) {
-                        if (!Service.Navmesh.IsRunning())
-                            Service.Navmesh.PathfindAndMoveTo(pos, false);
-                        else {
-                            Service.Navmesh.Stop();
-                            Service.Navmesh.PathfindAndMoveTo(pos, false);
-                        }
-                        return;
-                    }
+
+        var isPressed = IsKeyPressed(ECommons.Interop.LimitedKeys.LeftMouseButton) && Utils.IsClickingInGameWorld();
+        if (!wasPressed && isPressed)
+            wasPressed = true;
+        else if (wasPressed && !isPressed) {
+            wasPressed = false;
+            if (!Framework.Instance()->WindowInactive) {
+                Svc.GameGui.ScreenToWorld(ImGui.GetIO().MousePos, out var pos, 100000f);
+                if (Config.MovementType == MovementType.Pathfind) {
+                    if (Service.Navmesh.IsRunning()) Service.Navmesh.Stop();
+                    Service.Navmesh.PathfindAndMoveTo(pos, false);
+                }
+                else {
                     movement.Enabled = true;
-                    movement.DesiredPosition = destination = pos;
+                    movement.DesiredPosition = pos;
                 }
             }
         }

ea6264ba7faa840999c1029a12e13f9be87f2c7f
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2025-12-29 22:17:17 +0000

    finish FRA

--- ffxiv_bundledtweaks/Tweaks/FasterRepairAll.cs
+++ ffxiv_bundledtweaks/Tweaks/FasterRepairAll.cs
@@ -1,47 +1,55 @@
 ﻿using ECommons;
 using FFXIVClientStructs.FFXIV.Client.Game;
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 
 namespace ComplexTweaks.Tweaks;
 
 #if DEBUG
 [Tweak]
-public class FasterRepairAll : Tweak {
+public partial class FasterRepairAll : Tweak {
     public override string Name => "Faster Repair All";
     public override string Description => "Is this company stupid";
 
-    private readonly uint eventParamId = 0x43425400;
+    private const uint eventParamId = 0x43425400;
     public override void Enable() {
-        Svc.AddonLifecycle.RegisterListener(AddonEvent.PreReceiveEvent, "SelectYesno", CheckYes);
-        //Svc.AddonLifecycle.RegisterListener(AddonEvent.PostSetup, "Repair", AddEvent);
-        //Svc.AddonLifecycle.RegisterListener(AddonEvent.PreReceiveEvent, "Repair", HandleEvent);
-    }
-
-    private void CheckYes(AddonEvent type, AddonArgs args) {
-        if (((AddonReceiveEventArgs)args).EventParam == 1) // when you click no
-            ((AddonReceiveEventArgs)args).EventParam = 0; // make it impossible?
+        Svc.AddonLifecycle.RegisterListener(AddonEvent.PostSetup, "Repair", AddEvent);
+        Svc.AddonLifecycle.RegisterListener(AddonEvent.PreReceiveEvent, "Repair", HandleEvent);
     }
 
     public override void Disable() {
-        Svc.AddonLifecycle.UnregisterListener(CheckYes);
-        //Svc.AddonLifecycle.UnregisterListener(AddEvent);
-        //Svc.AddonLifecycle.UnregisterListener(HandleEvent);
+        Svc.AddonLifecycle.UnregisterListener(AddEvent);
+        Svc.AddonLifecycle.UnregisterListener(HandleEvent);
     }
 
     private unsafe void AddEvent(AddonEvent type, AddonArgs args) {
         var node = args.GetAddon<AtkUnitBase>()->GetNodeById<AtkResNode>(12);
-        node->AddEvent(AtkEventType.MouseClick, eventParamId, &args.GetAddon<AtkUnitBase>()->AtkEventListener, null, false);
+        node->AddEvent(AtkEventType.ButtonClick, eventParamId, &args.GetAddon<AtkUnitBase>()->AtkEventListener, null, false);
     }
 
     private void HandleEvent(AddonEvent type, AddonArgs args) {
-        if (args is AddonReceiveEventArgs { EventParam: var param }) {
-            if (param == eventParamId)
-                RepairAll();
-            else
-                ((AddonReceiveEventArgs)args).EventParam = 0;
+        if (args is not AddonReceiveEventArgs rea) return;
+        Log($"Received Repair Event: Type={rea.AtkEventType}, Param={rea.EventParam}");
+        if (rea is { AtkEventType: (byte)AtkEventType.ButtonClick, EventParam: 5 }) {
+            rea.AtkEventType = 0;
+            rea.EventParam = 0;
+        }
+        if (rea is { AtkEventType: (byte)AtkEventType.ButtonClick, EventParam: (int)eventParamId }) {
+            rea.AtkEventType = 0;
+            rea.EventParam = 0;
+            RepairAll();
         }
     }
 
-    private void RepairAll() => RepairCategory.Values.ToList().ForEach(inv => GameMain.ExecuteCommand(CommandFlag.RepairAllItemsNPC.Value, inv.Value));
+    private unsafe void RepairAll() {
+        if (AgentRepair.Instance()->IsSelfRepairOpen) { // TODO: offset on this might be wrong or it's the wrong thing entirely to check for
+            GameMain.ExecuteCommand(CommandFlag.RepairEquippedItems.Value, 1000);
+            RepairCategory.Values.ToList().ForEach(inv => GameMain.ExecuteCommand(CommandFlag.RepairAllItems.Value, inv.Value));
+        }
+        else {
+            GameMain.ExecuteCommand(CommandFlag.RepairEquippedItemsNPC.Value, 1000);
+            RepairCategory.Values.ToList().ForEach(inv => GameMain.ExecuteCommand(CommandFlag.RepairAllItemsNPC.Value, inv.Value));
+        }
+    }
 }
 #endif

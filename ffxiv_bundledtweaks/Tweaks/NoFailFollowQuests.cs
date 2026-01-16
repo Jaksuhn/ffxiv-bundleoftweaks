a6c76219f1613b0e14f2f1daad9e343beeed38aa
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2026-01-16 09:56:10 +0000

    fix sig
    for #96

--- ffxiv_bundledtweaks/Tweaks/NoFailFollowQuests.cs
+++ ffxiv_bundledtweaks/Tweaks/NoFailFollowQuests.cs
@@ -5,6 +5,6 @@ public partial class NoFailFollowQuests : Tweak {
     public override string Name => "No Fail Follow Quests";
     public override string Description => "Prevents being seen during follow quests (you can still be too far away).";
 
-    [SigHook("F3 0F 11 7C 24 ?? E8 ?? ?? ?? ?? 48 8B 9C 24 ?? ?? ?? ??")] // from atmo
+    [SigHook("E8 ?? ?? ?? ?? 84 C0 74 0F 8B 53 30")] // from atmo
     private bool FollowQuestRecast(nint a1, nint a2, nint a3, nint a4, nint a5, nint a6) => false;
 }

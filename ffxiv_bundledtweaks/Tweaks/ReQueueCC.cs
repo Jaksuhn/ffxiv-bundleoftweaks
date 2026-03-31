e347f5180a10f44b461e8c75343889fa2fec0bf9
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2026-03-31 19:37:51 +0100

    use new logmessage sub

--- ffxiv_bundledtweaks/Tweaks/ReQueueCC.cs
+++ ffxiv_bundledtweaks/Tweaks/ReQueueCC.cs
@@ -1,8 +1,6 @@
-﻿using Dalamud.Game.Text;
-using Dalamud.Game.Text.SeStringHandling;
+﻿using Dalamud.Game.Chat;
 using FFXIVClientStructs.FFXIV.Client.Game.UI;
 using FFXIVClientStructs.FFXIV.Client.UI.Agent;
-using Lumina.Excel.Sheets;
 
 namespace Automaton.Tweaks;
 
@@ -11,13 +9,12 @@ public class ReQueueCC : Tweak {
     public override string Name => "CC Error Requeue";
     public override string Description => "Requeues for Crystalline Conflict when your registration was cancelled due to a map change.";
 
-    public override void Enable() => Svc.Chat.ChatMessage += CheckForMessage;
-    public override void Disable() => Svc.Chat.ChatMessage -= CheckForMessage;
-    private unsafe void CheckForMessage(XivChatType type, int timestamp, ref SeString sender, ref SeString message, ref bool isHandled) {
-        if (type is XivChatType.ErrorMessage && message.Equals(LogMessage.GetRow(7392).Text)) {
-            if (AgentContentsFinder.Instance()->SelectedContent.FirstOrNull(x => x.ContentType is ContentsId.ContentsType.Roulette) is { Id: (40 or 41) and var id }) {
+    public override void Enable() => Svc.Chat.LogMessage += CheckForMessage;
+    public override void Disable() => Svc.Chat.LogMessage -= CheckForMessage;
+
+    private unsafe void CheckForMessage(ILogMessage message) {
+        if (message.LogMessageId is 7392)
+            if (AgentContentsFinder.Instance()->SelectedContent.FirstOrNull(x => x.ContentType is ContentsId.ContentsType.Roulette) is { Id: (40 or 41) and var id })
                 ContentsFinder.Instance()->QueueInfo.QueueRoulette((byte)id);
-            }
-        }
     }
 }
